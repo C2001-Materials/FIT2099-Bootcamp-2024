04ac80a46c2241ec7cf036b2e1d4599935ddb38b
alam0055 <alam0055@student.monash.edu>
2024-07-29 17:11:43 +0800

    Bootcamp Creation Removed 2 past .java files (testing files) Add initial MagicLibrary and Application classes
    - Created MagicLibrary class to manage the collection of magical books.
    
    - Created Application class to serve as the entry point for the system.
    
    - Set up basic structure and initial method calls for loading and displaying book details.

---
+++ src/MagicLibrary.java
@@ -0,0 +1,8 @@
+public class MagicLibrary {
+    public void printStatus() {
+        System.out.println("Welcome to the Hogwarts Library of FIT2099");
+        // createBooks();
+        // displayBooks();
+        System.out.println("Thank you for visiting Hogwarts Library of FIT2099!");
+    }
+}
