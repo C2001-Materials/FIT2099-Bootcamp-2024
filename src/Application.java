7c10c7c2c36b0ae8fec6c62138c7c37bf3d90193
alam0055 <alam0055@student.monash.edu>
2024-08-14 20:51:38 +0800

    Created merge request for Week 4 Branch into main

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,6 @@
 import edu.monash.fit2099bootcamp.MagicLibrary;
 
+// Driver class
 public class Application {
     public static void main(String[] args) {
         MagicLibrary magicLibrary = new MagicLibrary();
